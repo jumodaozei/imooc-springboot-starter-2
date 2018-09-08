a774b964a270870188eb28ac40c1fe7c79f0dea2
75c_100006496246_bastenyang <75c_100006496246_bastenyang@git.cloud.tencent.com>
2018-09-08 09:32:52 +0800

    第一次提交代码

---
+++ src/main/java/com/imooc/ImoocSpringbootStarter2Application.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSpringbootStarter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocSpringbootStarter2Application.class, args);
+	}
+}
